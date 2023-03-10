0aa7940ae37e545a7e9170e7efe96db084788ad9
Thảo GiBi <125585308+ThaoGiBiBehemoth@users.noreply.github.com>
2023-03-10 18:58:09 +0700

    1st: start & g model & set authorize to access

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,31 @@
+class ApplicationController < ActionController::API
+  def encode_token(payload)
+    payload[:exe] = 30.days.after.to_i
+    JWT.encode(payload, 'thaogibi')
+  end
+
+  def decode_token
+    auth_header = request.headers['Authorization']
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'thaogibi', true, alg: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def authorized_user
+    decoded_token = decode_token()
+    if decoded_token
+      user_id = decode_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def authorize
+    render json: { message: 'You must login to access.' }, status: 401 :unauthorized unless
+    authorized_user
+  end
+end
